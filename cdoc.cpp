923ef7b30aa8e48bc733562ef3c91908d07d733e
Kostya Stroykovskiy <bonkor@gmail.com>
2013-03-08 19:09:06 +0400

    calculateNextPos

--- cdoc.cpp
+++ cdoc.cpp
@@ -131,5 +131,168 @@ void CDoc::clearLastPay(quint8 pNu)
 
 quint8 CDoc::calculateNextPos(quint8 pNu, quint8 st)
 {
+    CPlayer *p = &m_p[pNu];
+    int curPos = p->pos;
 
+    if (st == 0) return curPos;
+
+    if (inCrest(curPos))
+    {
+        switch (p->crestDir) {
+        case D_Left:
+            if (curPos == 15)
+                curPos = 49 - st;
+            else {
+                curPos -= st;
+                if (curPos < 40) {
+                    if (p->stepsBack == 0)
+                        curPos = 39 - curPos + 35;
+                    else
+                        curPos = 35 + curPos - 39;
+                }
+            }
+            break;
+        case D_Right:
+            if (curPos == 35)
+                curPos = 39 + st;
+            else {
+                curPos += st;
+                if (curPos > 48) {
+                    if (p->stepsBack == 0)
+                        curPos = curPos - 49 + 15;
+                    else
+                        curPos = 15 - curPos + 49;
+                }
+            }
+            break;
+        case D_Up:
+            if (curPos == 25) {
+                curPos = 57 - st;
+                if (curPos == 52)
+                    curPos = 44;
+                else if (curPos == 51)
+                    curPos = 52;
+            } else if (curPos > 52 && curPos < 57) {
+                curPos -= st;
+                if (curPos == 52)
+                    curPos = 44;
+                else if (curPos > 47 && curPos < 52)
+                    curPos += 1;
+                else if (curPos == 47)
+                    curPos = 5;
+            } else if (curPos == 44) {
+                curPos = 53 - st;
+                if (curPos < 49) {
+                    if (p->stepsBack == 0)
+                        curPos = 48 - curPos + 5;
+                    else
+                        curPos = 5 + curPos - 48;
+                }
+            } else {
+                curPos -= st;
+                if (curPos < 49) {
+                    if (p->stepsBack == 0)
+                        curPos = 48 - curPos + 5;
+                    else
+                        curPos = 5 + curPos - 48;
+                }
+            }
+            break;
+        case D_Down:
+            if (curPos == 5) {
+                curPos = 48 + st;
+                if (curPos == 53)
+                    curPos = 44;
+                else if (curPos == 54)
+                    curPos = 53;
+            } else if (curPos > 48 && curPos < 53) {
+                curPos += st;
+                if (curPos == 53)
+                    curPos = 44;
+                else if (curPos > 53 && curPos < 58)
+                    curPos -= 1;
+                else if (curPos == 58)
+                    curPos = 25;
+            } else if (curPos == 44) {
+                curPos = 52 + st;
+                if (curPos > 56) {
+                    if (p->stepsBack == 0)
+                        curPos = curPos - 57 + 25;
+                    else
+                        curPos = 25 - curPos + 57;
+                }
+            } else {
+                curPos += st;
+                if (curPos > 56) {
+                    if (p->stepsBack == 0)
+                        curPos = curPos - 57 + 25;
+                    else
+                        curPos = 25 - curPos + 57;
+                }
+            }
+        }
+    } else
+    {
+        QString name = p->name;
+        int oldPos = curPos;
+        if (p->stepsBack == 0) {
+            curPos += st;
+            if (curPos > 39) curPos -= 40;
+            if (p->turnToStart > 0) {
+                if (oldPos < 5 && curPos > 5) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 49 + curPos - 6;
+                    if (curPos == 53) curPos = 44;
+                    p->crestDir = D_Down;
+                } else if (oldPos < 15 && curPos > 15) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 48 - curPos + 16;
+                    p->crestDir = D_Left;
+                } else if (oldPos < 25 && curPos > 25) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 56 - curPos + 26;
+                    if (curPos == 53) curPos = 44;
+                    p->crestDir = D_Up;
+                } else if (oldPos < 35 && curPos > 35) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 40 + curPos - 36;
+                    p->crestDir = D_Right;
+                }
+            }
+        } else {
+            curPos -= st;
+            if (curPos < 0) curPos += 40;
+            if (p->turnToStart > 0) {
+                if (oldPos > 5 && curPos < 5) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 49 - curPos + 4;
+                    if (curPos == 53) curPos = 44;
+                    p->crestDir = D_Down;
+                } else if (oldPos > 15 && curPos < 15) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 48 + curPos - 14;
+                    p->crestDir = D_Left;
+                } else if (oldPos > 25 && curPos < 25) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 56 + curPos - 24;
+                    if (curPos == 53) curPos = 44;
+                    p->crestDir = D_Up;
+                } else if (oldPos > 35 && curPos < 35) {
+                    emit sendLog(name + tr(" свернул к старту"));
+                    p->turnToStart--;
+                    curPos = 40 - curPos + 34;
+                    p->crestDir = D_Right;
+                }
+            }
+        }
+    }
+
+    return curPos;
 }
